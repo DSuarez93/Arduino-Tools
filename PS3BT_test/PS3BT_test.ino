bfea29bca0217d9de5541b45da6e6db8a66531f9
Damian <dsuarez1@stevens.edu>
2018-02-05 13:03:32 -0500

    Preferred test for USB BT
    Can be used to send digital pins low whne pressing a specific button. Connect an Arduino to the USB Host Shield with an external power supply.

--- PS3BT_test/PS3BT_test.ino
+++ PS3BT_test/PS3BT_test.ino
@@ -5,36 +5,47 @@
 USB Usb;
 BTD Btd(&Usb);
 PS3BT PS3(&Btd, 0x00, 0x15, 0x83, 0x3D, 0x0A, 0x57);
-bool switching;
+bool switching, sw1, sw2, sw3;
+const byte ledPin =   3;
+const byte testPin =  4;       //stop
+const byte move =     5;       //forward
+const byte ootw =     6;       //reverse
 
 void setup() {
   Serial.begin(115200);
-  switching = 0;
+  switching = 1; sw1 = 1; sw2 = 1; sw3 = 1;
   #if !defined(__MIPSEL__)
     while (!Serial);
   #endif
   if (Usb.Init() ==-1) {
     while(1) {
-      digitalWrite(LED_BUILTIN, switching);
+      digitalWrite(ledPin, switching);
       switching = !switching;
       delay(500);
     }
   }
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
+  pinMode(ledPin, OUTPUT);
   for (int i = 0; i<6; i++) {
-    digitalWrite(LED_BUILTIN, switching);
+    digitalWrite(ledPin, switching);
     switching = !switching;
     delay(200);
   }
 }
 
+void setHigh() {
+//digitalWrite(ledPin, HIGH);
+  digitalWrite(testPin, HIGH);
+  digitalWrite(move, HIGH);
+  digitalWrite(ootw, HIGH);
+  switching = 1; sw1=1; sw2=1; sw3 = 1;  
+}
+
 void loop() {
   Usb.Task();
+  setHigh();
   if (PS3.PS3Connected) {
-    if (PS3.getButtonClick(PS) && PS3.getButtonClick(L2) && PS3.getButtonClick(R1)) {
-      PS3.disconnect();
+    if (PS3.getButtonPress(PS) && PS3.getButtonPress(L2) && PS3.getButtonPress(R1)) {
+      PS3.disconnect(); digitalWrite(testPin, LOW);
     }
     if (PS3.getButtonClick(UP)) {
           PS3.setLedOff();
@@ -53,16 +64,32 @@ void loop() {
           PS3.setLedOn(LED3);
     }
     if (PS3.getButtonClick(SQUARE)) {
-          digitalWrite(5, switcher); switcher != switcher;
+        switching = !switching;
     }
     if (PS3.getButtonClick(TRIANGLE)) {
-          digitalWrite(4, switcher); switcher != switcher;
+        sw1 = !sw1;
     }
     if (PS3.getButtonClick(CROSS)) {
-          PS3.setRubmledOn(RumbleLow);
+        PS3.setRumbleOn(RumbleLow);
     }
     if (PS3.getButtonClick(CIRCLE)) {
-          PS3.setRumbleOn(RumbleHigh);
+        PS3.setRumbleOn(RumbleHigh);
     }
-  }    //end PS3 Connect
+    if (PS3.getButtonClick(R1)) {
+        sw2 = !sw2;
+    }
+    if (PS3.getButtonClick(L1)) {
+    }
+    if (PS3.getButtonClick(L3)) {
+    }
+    if (PS3.getButtonClick(R3)) {
+    }
+    if (PS3.getButtonClick(L2)) {
+    }    
+    if (PS3.getButtonClick(R2)) {
+    }    
+  }  else digitalWrite(testPin, LOW);  //end PS3 Connect
+  digitalWrite(ledPin, switching);
+  digitalWrite(move, sw1);
+  digitalWrite(ootw, sw2);
 }
